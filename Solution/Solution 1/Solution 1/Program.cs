e21a692445bf8d29e14e9a39bfeeeeff131add19
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2023-08-02 20:31:57 +0300

    Update Program.cs

--- Solution/Solution 1/Solution 1/Program.cs
+++ Solution/Solution 1/Solution 1/Program.cs
@@ -2,5 +2,6 @@
 int num = int.Parse(Console.ReadLine());
 for (int i = 0; i < num; i++)
 {
-    Console.WriteLine(("A" + i) + "Hello world");
+    Console.WriteLine(("A" + " " + i) + "Hello world");
+    Console.WriteLine(i++);
 }
