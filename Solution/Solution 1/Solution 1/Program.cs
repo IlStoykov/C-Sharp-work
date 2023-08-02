1d9918b526179cf30b0285ea2d8d1d5d11f143fb
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2023-08-02 20:26:48 +0300

    first in my hd

---
+++ Solution/Solution 1/Solution 1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num = int.Parse(Console.ReadLine());
+for (int i = 0; i < num; i++)
+{
+    Console.WriteLine(("A" + i) + "Hello world");
+}
